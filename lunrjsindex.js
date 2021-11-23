57183d1d0cb34e5c9a64258ebe58d18e7515fb0e
rdmueller <rdmueller@users.noreply.github.com>
2021-11-23 21:34:43 +0000

    deploy: 57b110b32bb3bb46284e89665db75ae3500ae92a

---
+++ lunrjsindex.js
@@ -0,0 +1,43 @@
+var documents = [
+
+{
+    "id": 0,
+    "uri": "02_about/index.html",
+    "menu": "about",
+    "title": "About",
+    "text": " Table of Contents About this project About this project lorem ipsum "
+},
+
+{
+    "id": 1,
+    "uri": "01_intro/010_readme.html",
+    "menu": "intro",
+    "title": "Readme",
+    "text": " Unresolved directive in &lt;stdin&gt; - include::../../README.adoc[leveloffset=+1] add some more info "
+},
+
+{
+    "id": 2,
+    "uri": "01_intro/020_architecture.html",
+    "menu": "intro",
+    "title": "Architecture",
+    "text": " Table of Contents Architecture Architecture Figure 1. basic overview /project recon_tools = ReconTools.new(arrays1, arrays2) assert_equal [], recon_tools.duplicate_data1_records, Should be 0 duplicates # (1) assert_equal [[c, 4, 6]], recon_tools.duplicate_data2_records, Should be 1 duplicate record assert_equal [a], recon_tools.matched_records.keys, Should be 1 matche keys: a assert_equal [a, 1, 3], (recon_tools.matched_records[a]), Check all elments of array match assert_equal [b], recon_tools.updated_records.keys, Should be 1 matche keys: b assert_equal [b, 2, 3], (recon_tools.updated_records[b]), Check all elments of array match describe "
+},
+
+{
+    "id": 3,
+    "uri": "search.html",
+    "menu": "-",
+    "title": "search",
+    "text": " Search Results "
+},
+
+{
+    "id": 4,
+    "uri": "lunrjsindex.html",
+    "menu": "-",
+    "title": "null",
+    "text": " will be replaced by the index "
+},
+
+];
